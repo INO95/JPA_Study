52ad20d26a7f3678dd36219e26678c5b7b68ac0e
INO <bih0905@naver.com>
2023-08-15 22:55:45 +0900

    first commit

---
+++ springBootMariaDbTest/src/main/java/com/example/springbootmariadbtest/SpringBootMariaDbTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmariadbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMariaDbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMariaDbTestApplication.class, args);
+    }
+
+}
---
+++ springBootMariaDbTest/src/test/java/com/example/springbootmariadbtest/SpringBootMariaDbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmariadbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMariaDbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
